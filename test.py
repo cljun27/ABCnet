e7d623b3bf96b4e14c22c0d013408d9385d691c5
cljun27 <52247368+cljun27@users.noreply.github.com>
2021-06-02 21:44:55 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,44 @@
+import time
+from options.test_options import TestOptions
+from data.data_loader import CreateDataLoader
+from util.visualizer import Visualizer
+
+from models.models import create_model
+from data import create_dataset
+from pdb import set_trace as st
+from util import html
+import os
+
+opt = TestOptions().parse()
+
+dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+dataset_size = len(dataset)    # get the number of images in the dataset.
+print('The number of training images = %d' % dataset_size)
+
+
+opt.nThreads = 1   # test code only supports nThreads = 1
+opt.batchSize = 1  # test code only supports batchSize = 1
+opt.serial_batches = True  # no shuffle
+opt.no_flip = True  # no flip
+
+# data_loader = CreateDataLoader(opt)
+# dataset = data_loader.load_data()
+model = create_model(opt)
+# create website
+web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.which_epoch))
+webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.which_epoch))
+# test
+id=0
+for i, data in enumerate(dataset):
+    if i >= opt.how_many:
+        break
+    model.set_input(data)
+    model.test()
+    visuals = model.get_current_visuals()
+    img_path = model.get_image_paths()
+    visualizer.save_images(webpage, visuals, img_path, id)
+    id = id+1
+    print('process image... %s' % img_path)
+    print(id)
+
+webpage.save()
